6c5d17d29e021defd0b545dd9108029d47de3721
Daniel Karlsson <donnen@gmail.com>
2020-06-24 08:54:41 -0700

    updates

--- exercise3.py
+++ exercise3.py
@@ -11,6 +11,8 @@ from sklearn.linear_model import LinearRegression
 from sklearn.linear_model import Ridge
 from sklearn.preprocessing import PolynomialFeatures
 from sklearn.pipeline import make_pipeline
+from sklearn.metrics import mean_squared_error
+from sklearn.metrics import r2_score
 import operator
 
 def pol2(x, A, B, C):
@@ -20,7 +22,7 @@ def MSE(yexp, ymod):
     return 1/len(yexp)*np.sum((yexp-ymod)**2)
 
 def R2(yexp, ymod):
-    return 1 - (np.sum((yexp-ymod)**2))/(np.sum((yexp - np.mean(yexp))**2))
+    return 1 - np.sum((yexp-ymod)**2)/np.sum((yexp - np.mean(yexp))**2)
 
 def RelativeError(yexp, ymod):
     return abs((yexp - ymod)/yexp)
@@ -60,7 +62,10 @@ print(MSE(y,ypredict))
 print(R2(y,ypredict))
 print(MSE(y,ytilde))
 print(R2(y,ytilde))
-
+print(mean_squared_error(y,ypredict))
+print(r2_score(y,ypredict))
+print(mean_squared_error(y,ytilde))
+print(r2_score(y,ytilde))
 
 
 
---
+++ exercise5.py
@@ -0,0 +1,18 @@
+#exercise 5
+
+import numpy as np
+import scipy
+import keras
+import matplotlib.pyplot as plt
+import pandas as pd
+import os
+import sys
+from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import Ridge
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.pipeline import make_pipeline
+from sklearn.metrics import mean_squared_error
+from sklearn.metrics import r2_score
+import operator
+
+
